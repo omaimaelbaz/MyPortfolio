4b6ff540ebf2ee118fd9702c03ac1bbc26c44fab
omaimaelbaz <oumacode01@gmail.com>
2024-04-04 16:33:17 +0200

    register auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    // auth user
+public function register(Request $request)
+{
+
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique',
+        'password' => 'required'
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+    ]);
+
+    // If validation passes and user is successfully created
+    if ($user) {
+        return response()->json([
+            'status' => true,
+            'message' => 'User Created Successfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ], 200);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Failed to create user',
+        ], 500);
+    }
+}
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $connection = 'mongodb';
-    protected $collection = 'Users_collection';
+    // protected $collection = 'Users_collection';
     protected $fillable = [
         'name',
         'email',
